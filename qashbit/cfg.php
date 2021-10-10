b7da1f42828e267a525f72cc9bdd20bc31ea7113
nguyenaman23 <91823952+nguyenaman23@users.noreply.github.com>
2021-10-10 14:10:56 +0700

    Add files via upload

---
+++ qashbit/cfg.php
@@ -0,0 +1,10 @@
+<?
+/**//**//**//**//**//**//**//**//**//**//*
+https://www.youtube.com/c/iewil
+Packet Tambahan
+$ pkg install tesseract 
+$ pkg install imagemagick
+*//**//**//**//**//**//**//**//**//**//**/
+$cookie = 'bitmedia_fid=eyJmaWQiOiJmY2QyNWNhODE1MDU2Yzg0MjVjZTk2ZjFiZTMzNGM3NiIsImZpZG5vdWEiOiI5ODY2N2Y3YjllN2RiNTNkOTYxNzg0ZTMzY2U5YWRkZiJ9; AccExist=54739; SesHashKey=8urbi928b3pcoebz; SesToken=ses_id%3D54739%26ses_key%3D8urbi928b3pcoebz; PHPSESSID=e6a3fb0rhfsgpo5m774nn7qu1p';
+$user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36';
+$url_solvmedia = 'https://api-secure.solvemedia.com/papi/_challenge.js?k=1yqk3nHfy5Px58G81cXjL7frVWm8h.KL;f=_ACPuzzleUtil.callbacks%5B0%5D;l=en;t=img;s=standard;c=js,h5c,h5ct,svg,h5v,v/h264,v/ogg,v/webm,h5a,a/mp3,a/ogg,ua/chrome,ua/chrome94,os/nt,os/nt10.0,fwv/BRfvkA.eevs76,jslib/jquery,htmlplus;am=-jNyOzCyb6v.JJ21MLJvqw;ca=ajax;ts=1633848309;ct=1633848299;th=white;r=0.5329464604163867';
